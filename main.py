2b71c853a4bf5c867029cd6dc64030f19ea965dc
Josh <dc.josh.kim@gmail.com>
2026-01-25 11:17:23 -0600

    TicTacToe Final

--- main.py
+++ main.py
@@ -9,8 +9,11 @@ while game._state:
     #     print("tie game")
     #     break
     game.loop()
+    print(game)
+    if game.checkstate() == "win" or game.checkstate() == "tie":
+        break
     game.aimove()
     print(game)
-    if game.checkstate() == "win" or game.checkstate == "tie":
+    if game.checkstate() == "win" or game.checkstate() == "tie":
         break
 print("game over!")
